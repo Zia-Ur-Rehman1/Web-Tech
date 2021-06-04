017847df17acd0cca99300def1d329cd9820c251
Khan Alone <ziakhanalone100@gmail.com>
2021-06-04 17:00:27 +0500

    Web Task flask basic

---
+++ flasky.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+# Creating an object  and passing modul name
+app = Flask(__name__)
+
+
+@app.route('/',method=['GET',"POST"])
+def hello():
+    return render_template ('index.html')
+
+
+@app.route('/home')
+def home():
+    return ("Home")
+    # <int:user_id> u can define datatype with it also
+@app.route('/user/<user_id>')
+def user(user_id):
+    return f"User id {user_id}"
+
+@app.route('/link')
+def link():
+    return '<a href="user/1122">User</a>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
